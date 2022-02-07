18a23cb5d95882b2a0bc7495bf7140d3d09938a0
aseemedakkad <aseemedakkad323@gmail.com>
2022-02-07 13:45:53 +0530

    6th commit

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from .import views
 
 urlpatterns =[
-    path('log',views.logFn,name='log'),
+    path('',views.logFn,name='log'),
     path('signup',views.sinpfn,name='signup'),
     path('home',views.homefn,name='home'),
     path('inbox',views.inboxfn,name='inbox'),
